0ce0266aeb381b213c57c9e609774cd5c0da77c4
Nicklas <nredheff@vols.utk.edu>
2017-09-26 21:24:38 -0400

    CBC is done minus deleting the padding.
    Signed-off-by: Nicklas <nredheff@vols.utk.edu>

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
 import binascii
-import sys
 import operator
 import os
 from Crypto.Cipher import AES
@@ -22,31 +21,39 @@ def encrypt(key, raw):
     if (raw is None) or (len(raw) == 0):
         raise ValueError('input text cannot be null or empty set')
     cipher = AES.new(key[:32], AES.MODE_ECB)
+
     ciphertext = cipher.encrypt(raw)
-    return binascii.hexlify(bytearray(ciphertext)).decode('utf-8')
+
+    return ciphertext
 
 
 def decrypt(key, enc):
+
     if (enc is None) or (len(enc) == 0):
         raise ValueError('input text cannot be null or empty set')
-    enc = binascii.unhexlify(enc)
-    cipher = AES.new(key[:32], AES.MODE_ECB)
+
+    # enc = binascii.unhexlify(enc)
+
+    cipher = AES.new(key, AES.MODE_ECB)
     enc = cipher.decrypt(enc)
-    return enc.decode('utf-8')
+    # return enc.decode('utf-8')
+    return enc
 
 def padding(raw):
+
     lraw = len(raw[-1])*8
-    modraw = lraw % 128
+    modraw = lraw % blocksize
     remainder = blocksize - modraw
-    # print("modraw:",modraw)
+    print("modraw:",modraw)
     if modraw == 0:
         raw.append(bytes("\0", encoding='utf-8') * int(((remainder)/(len(bytes("\0", encoding='utf-8')) * 8))))
     else:
         paddingstr = bytes(str(remainder),encoding='utf-8') * int(((remainder)/(len(bytes(str(remainder), encoding='utf-8')) * 8)))
         zerosreq = int((remainder %(len(bytes(str(remainder), encoding='utf-8')) * 8))/8)
-        # print("Zeros needed:",zerosreq)
+        print("Zeros needed:",zerosreq)
         zeros = bytes("\0", encoding='utf-8') * zerosreq
         raw[-1] = raw[-1] + zeros + paddingstr
+
     return raw
 
 def remove_padding(raw):
@@ -65,47 +72,46 @@ def chunks(l, n): # https://chrisalbon.com/python/break_list_into_chunks_of_e
         yield l[i:i+n]
 
 def IV_Gen():
-    return os.urandom(int(keysize/8))
+    return os.urandom(int(blocksize/8))
 
 def cbc_enc(key,raw):
     ct_split = []
     iv = IV_Gen()
     ct_split.append(iv)
     split_raw = list(chunks(raw,int(blocksize/8)))
-    print(split_raw)
+    # print("split raw", split_raw)
     padded_split_raw = padding(split_raw)
-
+    # print("padded_split_raw",padded_split_raw)
     for item in padded_split_raw:
-
         block = XOR(iv,item)
         iv = encrypt(key,block)
-        iv = bytes(iv, encoding='utf-8')
         ct_split.append(iv)
+
+    print("Enc:", ct_split)
     ct = b''.join(ct_split)
     return ct
 
 def cbc_dec(key,ct):
-    IV = ct[:32]
+    IV = ct[:16]
+    # print("IV:", IV)
     dt_split = []
-    ct = ct[32:]
-    split_raw = list(chunks(ct[32:],int(blocksize/8)))
-    print(split_raw)
-    for item in reversed(split_raw):
-        print(len(item))
-        block = decrypt(key,item)
-        dt_split.append(block)
-
-    for i in range(len(dt_split),0,-1):
-        print(i)
-
+    split_raw = list(chunks(ct[16:],int(blocksize/8)))
+    # print("Dec:",split_raw)
+    for i in range(0,len(split_raw)):
+        block = decrypt(key,split_raw[i])
+        if (i == 0):
+            dt_split.append(XOR(block,IV))
+        else:
+            dt_split.append(XOR(block, split_raw[i - 1]))
+    return b''.join(dt_split)
 
 if __name__ == "__main__":# Need some shit about the special way we are going to have him run our code
     key = bytes("1234567890abcdef1234567890abcdef", encoding='utf-8')
-    raw = bytes("1234567890abcdefabcdefghijklm", encoding='utf-8')
+    raw = bytes("1234567890abcdefabcdefghijklmnopq15001500", encoding='utf-8')
     print("raw: ",raw)
     # ct = encrypt(key, padding(raw))
     # dt = decrypt(key, ct)
     ct = cbc_enc(key,raw)
     dt = cbc_dec(key,ct)
-    print(ct)
-    print(dt)
+    print("CipherText:",ct)
+    print("PlainText: ",dt)

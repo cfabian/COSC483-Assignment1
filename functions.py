6baaf704b1fe1155eb7c79c48d682b9e461aed57
Nicklas <nredheff@vols.utk.edu>
2017-09-27 12:35:12 -0400

    CBC is working now.
    Signed-off-by: Nicklas <nredheff@vols.utk.edu>

--- functions.py
+++ functions.py
@@ -21,22 +21,15 @@ def encrypt(key, raw):
     if (raw is None) or (len(raw) == 0):
         raise ValueError('input text cannot be null or empty set')
     cipher = AES.new(key[:32], AES.MODE_ECB)
-
     ciphertext = cipher.encrypt(raw)
-
     return ciphertext
 
 
 def decrypt(key, enc):
-
     if (enc is None) or (len(enc) == 0):
         raise ValueError('input text cannot be null or empty set')
-
-    # enc = binascii.unhexlify(enc)
-
     cipher = AES.new(key, AES.MODE_ECB)
     enc = cipher.decrypt(enc)
-    # return enc.decode('utf-8')
     return enc
 
 def padding(raw):
@@ -44,22 +37,29 @@ def padding(raw):
     lraw = len(raw[-1])*8
     modraw = lraw % blocksize
     remainder = blocksize - modraw
-    print("modraw:",modraw)
+    # print("modraw:",modraw)
     if modraw == 0:
-        raw.append(bytes("\0", encoding='utf-8') * int(((remainder)/(len(bytes("\0", encoding='utf-8')) * 8))))
+        remainder = 128
+        paddingstr = (int(remainder/8)).to_bytes(int(remainder/8),byteorder="big",signed=False)* int(remainder/8)
+        # print("Padding str",paddingstr)
+        # raw.append(bytes("\0", encoding='utf-8') * int(((remainder)/(len(bytes("\0", encoding='utf-8')) * 8))))
+        raw.append(paddingstr)
     else:
-        paddingstr = bytes(str(remainder),encoding='utf-8') * int(((remainder)/(len(bytes(str(remainder), encoding='utf-8')) * 8)))
-        zerosreq = int((remainder %(len(bytes(str(remainder), encoding='utf-8')) * 8))/8)
-        print("Zeros needed:",zerosreq)
-        zeros = bytes("\0", encoding='utf-8') * zerosreq
-        raw[-1] = raw[-1] + zeros + paddingstr
+        # paddingstr = bytes(str(remainder),encoding='utf-8') * int(((remainder)/(len(bytes(str(remainder), encoding='utf-8')) * 8)))
+        paddingstr = (int(remainder / 8)).to_bytes(1, byteorder="big", signed=False) * int(remainder/8)
+        # print(paddingstr)
+        # print("Padding str", int(remainder / 8))
+        raw[-1] = raw[-1] + paddingstr
 
     return raw
 
 def remove_padding(raw):
     #needs some love
-    love = 1
-    return love
+    # print("raw",raw[-1][-1:])
+    i = int.from_bytes(raw[-1][-1:],byteorder="big",signed=False)
+    print(i)
+    raw[-1] = raw[-1][:16 - i]
+    return raw
 
 def XOR(a, b):# https://stackoverflow.com/questions/29408173/byte-operations-xor-in-python
     return bytes(map(operator.xor, a, b))
@@ -103,11 +103,13 @@ def cbc_dec(key,ct):
             dt_split.append(XOR(block,IV))
         else:
             dt_split.append(XOR(block, split_raw[i - 1]))
+    print(dt_split)
+    dt_split = remove_padding(dt_split)
     return b''.join(dt_split)
 
 if __name__ == "__main__":# Need some shit about the special way we are going to have him run our code
     key = bytes("1234567890abcdef1234567890abcdef", encoding='utf-8')
-    raw = bytes("1234567890abcdefabcdefghijklmnopq15001500", encoding='utf-8')
+    raw = bytes("1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdefads", encoding='utf-8')
     print("raw: ",raw)
     # ct = encrypt(key, padding(raw))
     # dt = decrypt(key, ct)
